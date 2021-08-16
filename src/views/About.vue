445936de71aa5efff8dddf7870ccbb05b0fa36d4
Emma Hokken <emmahokken@gmail.com>
2021-08-16 16:41:30 +0200

    initial view commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>This is a project that explored the use of Vue as a
+      front-end package and reshaprened my JavaScript skills. </p>
   </div>
 </template>
+
+<style scoped>
+.about {
+  text-align: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,72 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <Header @hide-form="hideAddTaskForm"/>
+      <div v-show="toggleAddTaskForm">
+        <AddTask @add-task="addTask"/>
+      </div>
+      <Tasks :tasks="tasks"/>
+      <Footer/>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Tasks from "@/components/Tasks";
+import AddTask from "@/components/AddTask";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Header,
+    Footer,
+    Tasks,
+    AddTask
   },
+  data: function () {
+    return {
+      tasks: [],
+      toggleAddTaskForm: false
+    }
+  },
+  methods: {
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
+    hideAddTaskForm() {
+      this.toggleAddTaskForm = !this.toggleAddTaskForm;
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 0,
+        text: "Meeting at home",
+        day: "March 4th, 2020 at 1:30pm",
+        reminder: true
+      },
+      {
+        id: 1,
+        text: "Meeting at ARTIS",
+        day: "March 13th, 2021 at 1:30pm",
+        reminder: false
+      },
+      {
+        id: 2,
+        text: "Record the end of the world",
+        day: "August 11th, 2021 at 4:00am",
+        reminder: true
+      },
+      {
+        id: 3,
+        text: "Feed the cat",
+        day: "July 4th, 2020 at 1:30pm",
+        reminder: true
+      },
+    ]
+  }
 };
 </script>
